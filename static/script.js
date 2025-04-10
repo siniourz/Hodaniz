d7a921b2510f5a0709079025ebd53d665f4d89f2
siniourz <kazemisina45@gmail.com>
2025-04-10 12:32:40 +0300

    all done v3

--- static/script.js
+++ static/script.js
@@ -44,6 +44,13 @@ function scrollToTop(event) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
 };
+function ScrollToFirstPart(event) {
+    event.preventDefault(); // Prevents the default jump
+    const section = document.getElementById('top');
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+};
 window.addEventListener('scroll', function() {
     const target = document.getElementById('moveToTopButton');
     if (window.scrollY > 1400) { // change the number to your desired scroll amount
@@ -54,4 +61,36 @@ window.addEventListener('scroll', function() {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const list = document.querySelector('.services-list');
+    const items = document.querySelectorAll('.services-list li');
+    const rightBtn = document.querySelector('.rightbutton');
+    const leftBtn = document.querySelector('.leftbutton');
 
+    let currentIndex = 0;
+    const itemsPerView = 4;
+    const totalItems = items.length;
+
+    function updateScroll() {
+        const itemWidth = items[0].getBoundingClientRect().width;
+        const scrollAmount = itemWidth * currentIndex;
+        list.style.transform = `translateX(-${scrollAmount+itemWidth/3}px)`;
+    }
+
+    rightBtn.addEventListener('click', () => {
+        if (currentIndex < totalItems - itemsPerView) {
+            currentIndex++;
+            updateScroll();
+        }
+    });
+
+    leftBtn.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateScroll();
+        } else {
+            list.style.transform = 'translateX(0px)'; // Reset to the start
+        }
+    });
+
+});
